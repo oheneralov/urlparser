dddb1ba2e1dbf9a16c16ac36867ad3abca873344
Alex <oheneralov@gmail.com>
2018-10-15 09:36:58 -0400

    splitting into 2 prts

--- main.cpp
+++ main.cpp
@@ -5,52 +5,78 @@
 #include <queue>
 using namespace std;
 
-int main(){
+std::ifstream::pos_type filesize(const char* filename)
+{
+    std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
+    return in.tellg(); 
+}
+
+int main(int argc, char* argv[]){
+if (filesize(argv[1]) < (450+1024*1024)){
+    cout << "file is too small and will not be split";
+    return 0;
+}
 string line;
 string prevLine;
 int counter = 0;
-int splitSize = 1000000; // 45 MGB
-std::ifstream infile("drug-label-0006-of-0007.json");
+int splitSize = 1000000; // 450 MGB
+std::ifstream infile(argv[1]);
+
+
+cout << argv[1] << " is spitting\n";
 ofstream myfile1 ("part1.json");
 ofstream myfile2 ("part2.json");
 queue<string> myqueue;
+bool isSecondPart = false;
 
 while (getline(infile, line))
 {
     counter++;
     istringstream iss(line);
+
+if (isSecondPart == false){
     myqueue.push(line);
     if (myqueue.size() > 3){
         myqueue.pop();
     }  
     // cout << line << "\n";
     if ((counter > splitSize) && (myqueue.front().find("},") != string::npos) && (prevLine.find("{") != string::npos) && (line.find("\"package_label_principal_display_panel\":") != string::npos) ){
-        cout << "found\n";
+        cout << counter << "is line where to split\n";
         myfile1 << "}\n]\n}" << "\n";
+        cout << "part1 iis ready\n";
         
-        break;
-    }
+        isSecondPart = true;
+        myfile2 << "{" << "\n";
+        myfile2 << "\"results\": [" << "\n";
+        myfile2 << "{" << "\n";
+       myfile2 << line << "\n";
+        
+        
+    } 
     prevLine = line;
     
    if (myfile1.is_open())
    {
     myfile1 << line << "\n";
     }
-  else {
-      cout << "Unable to open file";
+   else {
+      cout << "Unable to open the first file";
       break;
   }
-/*
-  if (counter > 1000){
-      cout << "not found\n";
-      break;
-   }
-*/
+} 
+// save the second part
+else{
+    
+    myfile2 << line << "\n";
+    
 
 }
 
-myfile1.close();
+}
 
+
+myfile1.close();
+myfile2                                                                                                                        .close();
 
 
 return 0;
